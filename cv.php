d4ff9649e328a9473f40c2543b2a2b4cf180c5e3
anburd <anburd@gmail.com>
2022-12-22 11:13:12 +0400

    22-12-22

--- cv.php
+++ cv.php
@@ -1,3 +1,26 @@
+<?php 
+$experience = [
+    [
+        'postCompany'=> 'типа Front-End Developer',
+        'dateFrom' => 'Янв. 2015',
+        'dateTo' => 'Current',
+        'duties' => 'PHP, JS, HTML, CSS и все остальное что было нужно '
+    ],
+   [
+        'postCompany'=> 'Web Developer / нескажемгде.ru',
+        'dateFrom' => 'Мар. 2012',
+        'dateTo' => 'Дек. 2014',
+        'duties' => 'грустно смотреть в монитор делая что вид что что-то понимаю'
+    ],
+    [
+        'postCompany'=> 'Graphic Designer / фриланс',
+        'dateFrom' => 'Июн. 2010',
+        'dateTo' => 'Мар. 2012',
+        'duties' => 'рисовать красивые картинки, говорить заказчикам 20 разными способами "пока не готово. будет через неделю"'
+    ],
+  ];
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -84,23 +107,17 @@
     
       <div class="w3-container w3-card w3-white w3-margin-bottom">
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+        <?php for ($i = 0; $i < count($experience); $i++): ?>
         <div class="w3-container">
-          <h5 class="w3-opacity"><b>Front End Developer </b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
-          <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+          <h5 class="w3-opacity"><b><?php echo $experience[$i]["postCompany"];?></b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $experience[$i]["dateFrom"];?> - 
+          
+          <?php if($experience[$i]["dateTo"] == 'Current') {?><span class="w3-tag w3-teal w3-round">Current</span><?php ;}
+          else { echo $experience[$i]["dateTo"];} ?>
+          <p><?php echo $experience[$i]["duties"];?></p>
           <hr>
         </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-          <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-          <hr>
-        </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-        </div>
+        <?php endfor;?>
       </div>
 
       <div class="w3-container w3-card w3-white">
